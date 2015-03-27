e9538497283100d7c98d3f3765bdeb05fd1c2435
31 <Durnus@gmail.com>
2015-03-26 20:12:24 -0500

    Rotate stuff, some inputs set.

---
+++ Assets/CHeliMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CHeliMovement : MonoBehaviour
+{
+	private float mainRotorRate = 0f;
+
+	private float tailRotorRateFraction = 1f;
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		float rollIn = Input.GetAxis("Roll");
+		float pitchIn = Input.GetAxis("Pitch");
+
+		//transform.Rotate(pitchIn, rollIn, 0f);
+		rigidbody.AddRelativeTorque(-pitchIn, rollIn, 0f);
+	}
+}
