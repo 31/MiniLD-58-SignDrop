e07fe8ca88eafff28b0c2238af4d68dde944776f
31 <Durnus@gmail.com>
2015-03-26 21:07:24 -0500

    Camera follows.

---
+++ Assets/Script/CFollowCam.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CFollowCam : MonoBehaviour
+{
+	public GameObject helo;
+
+	public Vector3 offset;
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		Vector3 from = helo.transform.TransformPoint(offset);
+		Vector3 look = helo.transform.position;
+
+		transform.position = Vector3.Lerp(transform.position, from, Time.deltaTime);
+		transform.LookAt(look);
+	}
+}
--- Assets/Script/CHeliMovement.cs
+++ Assets/Script/CHeliMovement.cs
@@ -16,10 +16,19 @@ public class CHeliMovement : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
+	}
+
+	void FixedUpdate()
+	{
 		float rollIn = Input.GetAxis("Roll");
 		float pitchIn = Input.GetAxis("Pitch");
 
 		//transform.Rotate(pitchIn, rollIn, 0f);
 		rigidbody.AddRelativeTorque(-pitchIn, rollIn, 0f);
+
+		// Relative wind speed squared.
+		float windSpeed2 = rigidbody.velocity.sqrMagnitude;
+
+		rigidbody.AddRelativeForce(9.81f * Vector3.up * 1.05f);
 	}
 }
