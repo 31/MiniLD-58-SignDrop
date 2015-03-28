032aec47c56804ca87434ad58517b71aecee6667
31 <Durnus@gmail.com>
2015-03-28 14:09:15 -0500

    Add water that pulls you down.

---
+++ Assets/Script/CWaterSlow.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CWaterSlow : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+	}
+
+	void OnTriggerStay(Collider other)
+	{
+		// Slow down the intruder.
+		var otherBody = other.GetComponentInParent<Rigidbody>();
+
+		otherBody.AddForceAtPosition(
+			-otherBody.velocity * 50f,
+			other.transform.position);
+
+	}
+}
